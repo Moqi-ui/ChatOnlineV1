6ae837e266f86ffb1c3b134e7a13069dea705bc9
Mrcao21 <740252408@qq.com>
2021-01-26 21:37:47 +0800

    添加GMESDK，因为GME插件不会更改，所以不用管理上传-imrcao 0126

--- ChatOnline_WFY/Source/ChatOnline_WFY/ChatOnline_WFY.Build.cs
+++ ChatOnline_WFY/Source/ChatOnline_WFY/ChatOnline_WFY.Build.cs
@@ -8,6 +8,6 @@ public class ChatOnline_WFY : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay","GMESDK","UMG" });
 	}
 }
